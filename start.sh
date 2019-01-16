5a0244e55c049ed9ed24e59dbc2cfac5ac44e0bf
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-01-16 23:53:11 +0900

    𓃠

--- start.sh
+++ start.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+# colors
+COLOR_R="\033[0;31m"    # red
+COLOR_G="\033[0;32m"    # green
+COLOR_Y="\033[0;33m"    # yellow
+COLOR_B="\033[0;34m"    # blue
+COLOR_M="\033[0;35m"    # magenta
+COLOR_C="\033[0;36m"    # cyan
+
+# reset
+COLOR_RESET="\033[00m"
+
+
 function show_banner {
     local output=$(cat .banner)
     echo -e "$COLOR_B$output$COLOR_RESET"
